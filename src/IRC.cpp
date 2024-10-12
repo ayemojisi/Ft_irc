d58d40eca3e408e46067a108821bcfbff0e079dc
Melih Altun <maltun@k1m22s01.42istanbul.com.tr>
2024-10-12 13:29:33 +0300

    bitecek

--- src/IRC.cpp
+++ src/IRC.cpp
@@ -6,8 +6,7 @@
 #include <string>
 #include <sys/select.h>
 
-IRC::IRC(int port, string password)
-{
+IRC::IRC(int port, string password) {
   this->port = port;
   this->password = password;
   this->maxClients = MAXCLIENTS;
@@ -38,36 +37,27 @@ IRC::IRC(int port, string password)
        << endl;
 };
 
-IRC::IRC(const IRC &irc)
-{
-  if (this != &irc)
-  {
+IRC::IRC(const IRC &irc) {
+  if (this != &irc) {
     this->port = irc.port;
     this->password = irc.password;
   }
 };
 
-void IRC::start()
-{
-  while (true)
-  {
+void IRC::start() {
+  while (true) {
     this->readfd = this->masterfd;
     int selectStatus = select(this->maxfd + 1, &this->readfd, NULL, NULL, NULL);
-    switch (selectStatus)
-    {
-    case (0):
-    {
+    switch (selectStatus) {
+    case (0): {
       cout << FG_YELLOW << "Select Timeout..." << RESET << endl;
       break;
     }
     case (-1):
       throw runtime_error("Select error");
-    default:
-    {
-      for (int i = 0; i <= this->maxfd; i++)
-      {
-        if (FD_ISSET(i, &this->readfd))
-        {
+    default: {
+      for (int i = 0; i <= this->maxfd; i++) {
+        if (FD_ISSET(i, &this->readfd)) {
           if (i == this->sockfd)
             newClientAdd();
           else
@@ -80,15 +70,13 @@ void IRC::start()
   }
 }
 
-void IRC::newClientAdd()
-{
+void IRC::newClientAdd() {
   int clientLen = sizeof(this->clientAddr);
   if ((this->newClientfd =
            accept(this->sockfd, (struct sockaddr *)&this->clientAddr,
                   (socklen_t *)&clientLen)) == -1)
     throw runtime_error("Accept error");
-  else
-  {
+  else {
     FD_SET(this->newClientfd, &this->masterfd);
     if (this->newClientfd > this->maxfd)
       this->maxfd = this->newClientfd;
@@ -97,79 +85,80 @@ void IRC::newClientAdd()
   }
 }
 
-static void handleClientQuit(int sockfd, fd_set &masterfd, map<int, Client> &clients)
-{
+static void handleClientQuit(int sockfd, fd_set &masterfd,
+                             map<int, Client> &clients) {
   Client &client = clients.find(sockfd)->second;
-  cout << FG_RED << "[" << sockfd << "] " << (client.getNickname().empty() ? "client" : client.getNickname()) << " is Disconnected"<< endl << RESET;
+  cout << FG_RED << "[" << sockfd << "] "
+       << (client.getNickname().empty() ? "client" : client.getNickname())
+       << " is Disconnected" << endl
+       << RESET;
   close(sockfd);
   FD_CLR(sockfd, &masterfd);
   clients.erase(sockfd);
 }
 
-void IRC::handleClient(int sockfd)
-{
+void IRC::handleClient(int sockfd) {
   Client &client = this->clients.find(sockfd)->second;
   char buff[BUFFER_SIZE] = {0};
   int nbytes;
-  if ((nbytes = recv(client.getSockfd(), buff, sizeof(buff) - 1, 0)) <= 0)
-  {
+  if ((nbytes = recv(client.getSockfd(), buff, sizeof(buff) - 1, 0)) <= 0) {
     if (nbytes != 0)
-      throw runtime_error("Recv error");
+      cout << FG_RED + client.getUsername() + " its Quit" + RESET;
     handleClientQuit(sockfd, this->masterfd, this->clients);
-  }
-  else
-  {
+  } else {
     std::istringstream iss(buff);
     string token, result;
     buff[nbytes] = '\0';
-    while (iss >> token)
-    {
-      if (token == "NICK")
-      {
+    while (iss >> token) {
+      if (token == "NICK") {
         iss >> result;
         client.setNickname(result);
-      }
-      else if (token == "PASS")
-      {
+      } else if (token == "PASS") {
         iss >> result;
         client.setPassword(result);
-      }
-      else if (token == "USER")
-      {
+      } else if (token == "USER") {
         iss >> result;
         client.setUsername(result);
       }
-      //else if(token == "PING")
-      else if(!client.getIsAuthed())
-      {
-        if((token != "PRIVMSG" && token != "JOIN" && token != "TOPIC" && token != "KICK" && token != "INVITE" && token != "LIST" && token != "MODE" && token != "PART" && token != "QUIT"))
-					sendMsg(client.getSockfd(), client.getNickname() + "COMMAND 1NOT FOUND.\r\n");
-				else
-					sendMsg(client.getSockfd(), client.getNickname() + "PLEASE LOGIN FIRST.\r\n");
-				while(iss >> token);
+      // else if(token == "PING")
+      else if (!client.getIsAuthed()) {
+        if ((token != "PRIVMSG" && token != "JOIN" && token != "TOPIC" &&
+             token != "KICK" && token != "INVITE" && token != "LIST" &&
+             token != "MODE" && token != "PART" && token != "QUIT"))
+          sendMsg(client.getSockfd(),
+                  client.getNickname() + "Command is not found.\r\n");
+        else
+          sendMsg(client.getSockfd(), FG_LIGHTRED + client.getNickname() +
+                                          " Please login!\r\n" + RESET);
+        while (iss >> token)
+          ;
       }
-			if(!client.getPassword().empty() && !client.getUsername().empty() && !client.getNickname().empty() && !client.getIsAuthed()){
-				std::string loginCommand = ": 001 " + client.getNickname() + " :Welcome to the IRC Network " + client.getNickname() + "!" + client.getUsername() + "@" + "\r\n";
-        		sendMsg(client.getSockfd(), loginCommand);
-				client.setIsAuthed(true);
-				continue;
-			}
+      if (!client.getPassword().empty() && !client.getUsername().empty() &&
+          !client.getNickname().empty() && !client.getIsAuthed()) {
+        sendMsg(client.getSockfd(),
+                ": 001 " + client.getUsername() + ". Hello Everybody.");
+        client.setIsAuthed(true);
+        continue;
+      }
 
       /* Bunlar eklenecek
       if(client.getIsAuth() && token == "PRIVMSG")
-			else if(client.getIsAuth() && token == "JOIN")
-			else if(client.getIsAuth() && token == "TOPIC")
-			else if(client.getIsAuth() && token == "KICK")
-			else if(client.getIsAuth() && token == "LIST")
-			else if(client.getIsAuth() && token == "INVITE")
-			else if(client.getIsAuth() && token == "MODE")
-			else if(client.getIsAuth() && token == "PART")
-			else if(client.getIsAuth() && token == "QUIT")
-			else if(client.getIsAuth() && token == 	"NOTICE")
-			else if(client.getIsAuth()){
+                        else if(client.getIsAuth() && token == "JOIN")
+                        else if(client.getIsAuth() && token == "TOPIC")
+                        else if(client.getIsAuth() && token == "KICK")
+                        else if(client.getIsAuth() && token == "LIST")
+                        else if(client.getIsAuth() && token == "INVITE")
+                        else if(client.getIsAuth() && token == "MODE")
+                        else if(client.getIsAuth() && token == "PART")
+                        else if(client.getIsAuth() && token == "QUIT")
+                        else if(client.getIsAuth() && token == 	"NOTICE")
+                        else if(client.getIsAuth()){
       */
     }
-    cout << FG_CYAN << "[" << sockfd << "] " << (client.getNickname().empty() ? "client" : client.getNickname()) << " : " << FG_WHITE << buff << endl << RESET;
+    cout << FG_CYAN << "[" << sockfd << "] "
+         << (client.getNickname().empty() ? "client" : client.getNickname())
+         << " : " << FG_WHITE << buff << endl
+         << RESET;
     sendAllClientMsg(this->clients, buff);
   }
 }
